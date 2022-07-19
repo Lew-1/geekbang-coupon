616b36e2edd00994a3347b188dad9424a47f5476
luzhonghe <1056643337@qq.com>
2022-07-19 11:33:45 +0800

    完善

---
+++ coupon-template-serv/coupon-template-api/src/main/java/com/geekbang/coupon/template/api/beans/CouponInfo.java
@@ -0,0 +1,30 @@
+package com.geekbang.coupon.template.api.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author luzhonghe
+ * @date 2022/7/18 0:00
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CouponInfo {
+
+    private Long id;
+
+    private Long templateId;
+
+    private Long userId;
+
+    private Long shopId;
+
+    private Integer status;
+
+    private CouponTemplateInfo template;
+
+}
